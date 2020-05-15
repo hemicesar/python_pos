b40ffc86034dae6c72415782da31884a1643a992
hemicesar <65261495+hemicesar@users.noreply.github.com>
2020-05-15 04:01:40 -0500

    Update 01
    working at night

--- edos_financieros.py
+++ edos_financieros.py
@@ -1,7 +1,12 @@
+# ------------------------
+# Nuevos objetivos
+# Hacer variable y ajustar enmemoria el capital inicial.
+# Comenzar a generar base de datos para diferenciar los productos que se trabajan.
+# Capturar los datos directamete del lector de código de barras.
+# Pasar el algoritmo a que trabaje con tecnologías web.
+# Hacer visible el programa desde una terminal móvil.
 
-
-
-
+suma_ventas = 0
 # ------------------------
 # Ciclo principal del Programa
 continuar = True
@@ -15,26 +20,41 @@ while continuar:
 
 # ------------------------
 # Definición de variables
-
-  venta = float(input("\t Ingresa el monto de la venta: "))
-  print("El valor de tu venta es: ", venta)
+  capital_inicial = 500
+  num_articulo = 0
+  num_venta = 0
+  venta = 0
 #  n = int(input("\n¿Hasta que posición de la serie de fibonacci quiere ver? "))
 #  c = k = j = 1
 #  l = [k]
-  cantidad_vtas = 315
-  #monto_vtas =
+
 # ------------------------
-# Algoritmo que calcula la serie de Fibonacci
+# Algoritmo que calcula el monto de una venta
+  print ("\nSu capital inicial es: ", capital_inicial)
+
   vendo = True
   while vendo:
+    articulo = float(input("\t\n Ingrese el valor del artículo: "))
+    print("Valor del producto: ", articulo)
+    num_articulo += 1
     #k,j = j, k + j
     #l.append(k) # Almacena el resultado en una lista
     #c = c + 1
-    venta = venta + cantidad_vtas
-    vendo = ("S" == input("\n¿Desea registrar otra venta? S/n "))
+    venta = articulo + venta
+    vendo = ("s" == input("\nPresione s para registrar artículo adicional "))
+    num_venta += 1
+    print("\nArtículos vendidos: ", num_articulo)
+    print("\nMonto de la venta: ", venta)
 # ------------------------
 # Imprime el resultado en pantalla
-  #print (l)
 
-  print("El número de ventas realizadas fue de: ", venta)
+
+  suma_ventas = venta + suma_ventas
+  capital_total = capital_inicial + suma_ventas
+  print ("\nVentas realizadas: ", num_venta)
+  print ("\nMonto ventas: ", suma_ventas)
+  print ("\nSu nuevo capital es:", capital_total)
+
   continuar = ("s" == input("\nPresione s para registrar una nueva venta "))
+
+print ("\n\tExelente trabajo!  Nos vemos mañana.")
